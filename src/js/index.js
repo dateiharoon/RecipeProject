1360354e26887e5f8d83dd9d3994ddafd4675a50
Haroon Iqbal <dateiharoon@gmail.com>
2020-01-05 23:39:32 +0000

    fourth Commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,8 @@
 // Global app controller
-import num from './test';
-const x = 23;
-console.log(`I imported ${num} from another module called test! Variable x is ${x} `);
+// import num from './test';
+// const x = 23;
+// console.log(`I imported ${num} from another module called test! Variable x is ${x} `);
+
+import str from './models/Searh';
+
+i9mport {add, multiply} from '.'
