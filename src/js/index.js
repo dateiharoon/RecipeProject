4546917b9ca7c136d05baa8da538da65fc2f335c
Haroon Iqbal <dateiharoon@gmail.com>
2020-01-10 00:00:08 +0000

    seventh Commit

--- src/js/index.js
+++ src/js/index.js
@@ -1,12 +1,18 @@
-// Global app controller
-// import num from './test';
-// const x = 23;
-// console.log(`I imported ${num} from another module called test! Variable x is ${x} `);
+// // Global app controller
+// // import num from './test';
+// // const x = 23;
+// // console.log(`I imported ${num} from another module called test! Variable x is ${x} `);
 
-import str from './models/Search';
+// import str from './models/Search';
 
-//import {add as a,  multiply as m, ID } from './views/searchView';
+// //import {add as a,  multiply as m, ID } from './views/searchView';
 
-import * as searchView from './views/searchView';
+// import * as searchView from './views/searchView';
 
-console.log(`Using important functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
+// console.log(`Using important functions! ${searchView.add(searchView.ID, 2)} and ${searchView.multiply(3, 5)}. ${str}`);
+
+async function getResults(query){
+
+}
+
+getResults
